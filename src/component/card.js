63c4f3432edcdbf4536354cd765348efd9d8c051
Santosh Kumar <santoshkumar964887@gmail.com>
2020-03-14 00:38:32 +0530

    adding random pic

--- src/component/card.js
+++ src/component/card.js
@@ -4,7 +4,10 @@ import './card.style.css';
 export const Card=(props)=>{
     return (
     <div className="card">
-    <img src="https://joeschmoe.io/api/v1/random"/>
+        
+            
+        
+    <img src={`https://joeschmoe.io/api/v1/${props.list.name}`}/>
     <h3 >Name : {props.list.name}</h3>
     <h5>Email : {props.list.email}</h5>
     <p>Number : {props.list.phone}</p>
