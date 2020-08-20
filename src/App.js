631a39b7215ff1eacfefdba7556c51b9a3e1b518
Santosh Kumar <santoshkumar964887@gmail.com>
2020-08-20 16:30:35 +0530

    adding functionlity dynamic pic

--- src/App.js
+++ src/App.js
@@ -1,42 +1,40 @@
-import React,{Component} from 'react';
-import './App.css';
-import {CardList} from './component/cardlist';
-class App extends Component{
-  constructor(){
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
     super();
     this.state = {
       list: [],
-      serch : ""
+      serch: "",
     };
-    
   }
-  
-  
-componentDidMount(){
-  fetch('https://jsonplaceholder.typicode.com/users').then(response => response.json()).then(user => this.setState({list:user}));
 
-   
-}
-render(){
-  const {list,serch}=this.state;
-  const list1 = list.filter(list=>list.name.toLowerCase().includes(serch.toLowerCase()));
-  
-  return (
-    <div className="cl2">
-      <div className="cl1">
-      <input type="text" placeholder="Search Monsters By Name" onChange={e=>{this.setState({serch : e.target.value})
-    console.log(this.state.serch)}
-    }/>
-    </div>
-      <CardList list={list1} />
-     
-      
-  </div>)
-    
-}
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((user) => this.setState({ list: user }));
+  }
+  render() {
+    const { list, serch } = this.state;
+    const list1 = list.filter((list) =>
+      list.name.toLowerCase().includes(serch.toLowerCase())
+    );
 
-
-
-
+    return (
+      <div className="cl2">
+        <div className="cl1">
+          <input
+            type="text"
+            placeholder="Search Monsters By Name"
+            onChange={(e) => {
+              this.setState({ serch: e.target.value });
+              console.log(this.state.serch);
+            }}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 export default App;
--- src/component/cardlist.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import'./liststyle.css';
-import {Card} from './card';
-
-export const CardList=(props)=>{
-return (<div className="cardlist"> {props.list.map(list=><Card key={list.id} list={list}/>)}</div>)
-}
